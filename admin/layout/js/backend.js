1524a9e489205eaddbcdb12f775c9c289bb250f3
Ahmed Maky <129126532+ahmedmansour999@users.noreply.github.com>
2024-02-18 00:21:42 +0200

    recover

---
+++ admin/layout/js/backend.js
@@ -0,0 +1,74 @@
+$(document).ready(function () {
+  $("[placeholder]")
+    .focus(function () {
+      $(this).attr("text-data", $(this).attr("placeholder"));
+      $(this).attr("placeholder", "");
+    })
+    .blur(function () {
+      $(this).attr("placeholder", $(this).attr("text-data"));
+    });
+
+
+  // /*
+  //   add * to required attributes
+  // */
+
+  //   $("input").blur(function () {
+  //     if ($(this).val().trim() === "") {
+  //         $(this).after('<span class="asterisk">*Required</span>');
+  //         $(this).css("background-color", "red");
+  //     }
+  // }).keypress(function(){
+  //     // Remove asterisk if user starts typing
+  //     $(this).next('.asterisk').remove();
+  //     $(this).css("background-color", "#EEE");
+  // });
+
+  
+  /*
+    Confirm Delete 
+  */
+
+  $(".confirm").click(function () {
+    return confirm("Are you sure ? ");
+  });
+
+
+
+  /*
+      show password
+  */
+
+  var passField = $(".password");
+
+  $(".show-pass").click(function () {
+    var passField = $(".password");
+    if (passField.attr("type") === "password") {
+      passField.attr("type", "text");
+    } else {
+      passField.attr("type", "password");
+    }
+  });
+
+
+
+  $(".cat .head").click(function() {    
+    $(this).next(".cat .body").fadeToggle(100); 
+
+  });
+  
+
+
+
+
+  $('.view a').click(function() {
+    $(this).addClass('active').siblings('a').removeClass('active') ;
+    if ($(this).data('view') == "full") {
+      $(".cat .body").fadeIn(100);
+    } else{
+      $('.cat .body').fadeOut(100);
+    }
+  })
+
+
+});
